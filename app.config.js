49092c93479efd71c4cc9f40e5017288f3720484
Carroll Wen <carroll.wen@iqair.com>
2025-10-30 09:29:32 +0800

    update app.config.js

---
+++ app.config.js
@@ -0,0 +1,10 @@
+export default ({ config }) => {
+  return {
+    ...config, // keep everything from app.json
+    extra: {
+      ...config.extra, // preserve existing extra if any
+      EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
+      EXPO_PUBLIC_SUPABASE_KEY: process.env.EXPO_PUBLIC_SUPABASE_KEY,
+    },
+  };
+};
