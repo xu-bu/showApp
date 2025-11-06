483fff40778e129e33ede20a88ed9cc13158c32b
Carroll Wen <carroll.wen@iqair.com>
2025-11-06 16:24:48 +0800

    fix image modal

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react';
-import { View, Button, ActivityIndicator, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { View, Button, ActivityIndicator, Text, ScrollView, Image } from 'react-native';
 import { getTokenConfig, getListConfig } from '../requestConfig';
 import { injectRequestConfig } from '../injectRequestConfig';
 import { Picker } from '@react-native-picker/picker';
@@ -10,7 +10,8 @@ import { request } from '../services/request';
 import _ from 'lodash'
 import { CONCURRENT_REQUESTS_NUM } from '../consts'
 import { PickerStyles } from '../styles/styles'
-import ModalImage from "react-modal-image";
+import Zoom from "react-medium-image-zoom";
+import 'react-medium-image-zoom/dist/styles.css'
 
 interface ActivityData {
   title: string;
@@ -26,7 +27,6 @@ export default function App() {
   const [data, setData] = useState<ActivityData[] | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const [currentImage, setCurrentImage] = useState<{ uri: string } | null>(null);
   const [selectedCity, setSelectedCity] = useState('');
   const [selectedArtist, setSelectedArtist] = useState('');
   const fetchData = async () => {
@@ -84,6 +84,7 @@ export default function App() {
     if (!data) return [];
     let filteredEvents = data;
 
+    console.log(selectedCity)
     if (selectedCity) filteredEvents = filteredEvents.filter(item => item.city === selectedCity);
     if (selectedArtist) filteredEvents = filteredEvents.filter(item => item.artist === selectedArtist);
 
@@ -146,19 +147,12 @@ export default function App() {
               <Text>{item.city}</Text>
               <Text>{item.showTime}</Text>
               <Text>{item.siteName}</Text>
-              <TouchableOpacity onPress={() => { setCurrentImage({ uri: item.avatar }); }}>
+              <Zoom>
                 <Image
                   source={{ uri: item.avatar }}
                   style={{ width: 100, height: 100, borderRadius: 50, marginTop: 10 }}
                 />
-              </TouchableOpacity >
-              {currentImage && (
-                <ModalImage
-                  small={currentImage.uri}
-                  large={currentImage.uri}
-                  alt=""
-                />
-              )}
+              </Zoom>
             </View>
           ))}
         </ScrollView>
